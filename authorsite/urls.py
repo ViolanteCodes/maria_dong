10094ccf0ef3af491bafcb475046346fa4772ea8
ViolanteCodes <maria@buttercms.com>
2022-03-16 16:43:27 -0400

    started tailwind layout

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from landing import views
+
 
 urlpatterns = [
+    path('', views.landing_page),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def landing_page(request):
+    """View to return landing Page"""
+    return render(request, 'landing_page.html', {})
