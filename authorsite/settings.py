94a7c90795199e06db1ec44e82e33fc994d1170f
ViolanteCodes <maria@buttercms.com>
2022-03-17 12:43:58 -0400

    move landing app to layout

--- authorsite/settings.py
+++ authorsite/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
     # apps
     'books',
-    'landing',
+    'layout',
 ]
 
 MIDDLEWARE = [
--- authorsite/urls.py
+++ authorsite/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from landing import views
+from layout import views
 
 
 urlpatterns = [
