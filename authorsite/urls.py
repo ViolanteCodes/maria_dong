5e1b82fe711561d474a66e8fa2d7c2ca778c22f9
ViolanteCodes <maria@buttercms.com>
2022-04-20 21:17:02 -0400

    convert butter pages to CBV

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -4,7 +4,10 @@ from layout.views import ButterView
 
 
 urlpatterns = [
-    path('', ButterView.as_view()),
+    path('', ButterView.as_view(template_name="landing_page.html"),
+        kwargs={'page_slug': 'landing-page'}),
+    path('books/<page_slug>/', ButterView.as_view(template_name='book_detail.html'),
+        name="book_page"),
     # path('books/<book_slug>/', views.book_detail_page, name="book_detail_page"),
     # path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
--- layout/views.py
+++ layout/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.http import Http404
 from django.views.generic import TemplateView
 
 # Import Butter
@@ -14,16 +14,30 @@ pp = pprint.PrettyPrinter(indent=4)
 
 class ButterView(TemplateView):
     """Base Butter Page View"""
-    template_name = "landing_page.html"
 
-    def get_context_data(self, **kwargs):
-        """Context Data"""
-        context = super().get_context_data(**kwargs)
+    def get(self, request, page_slug, *args, **kwargs):
+        context = self.get_context_data(page_slug, *args, **kwargs)
+        return self.render_to_response(context)
+
+
+    def get_context_data(self, page_slug, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        # Get Menu
         if 'main_menu' not in self.kwargs or self.kwargs['main_menu'] != false:
             main_menu = self.get_menu()
-        context['nav_menu'] = main_menu
-        page_data = self.get_page()
-        context['data'] = page_data
+            context['nav_menu'] = main_menu
+        # Get Params or set default
+        if 'params' in self.kwargs:
+            params = self.kwargs['params']
+        else:
+            params = {
+                'page': '1',
+                'page_size': '10',
+                'levels': '3',
+            }
+        # fetch page_data
+        page_data = self.get_page(page_slug=page_slug, params=params)
+        context['page_data'] = page_data
         return context
 
     def get_menu(self):
@@ -35,53 +49,7 @@ class ButterView(TemplateView):
         nav_menu = Butter.content_fields.get(['navigation_menu'], params)
         return nav_menu['data']['navigation_menu'][0]['menu_items']
 
-    def get_page(self, page_type='*', page_slug="landing-page", params={}):
+    def get_page(self, page_type='*', page_slug="", params={}):
         """Fetch a page from butter"""
-        params = {
-        'page': '1',
-        'page_size': '10',
-        'levels': '3',
-        }
         page_data = Butter.pages.get(page_type, page_slug, params)['data']['fields']
         return page_data
-
-def get_menu():
-    """Function to retrieve main navigation menu from butter"""
-    params = {
-    'page': '1',
-    'page_size': '10'
-    }
-    nav_menu = Butter.content_fields.get(['navigation_menu'], params)
-    return nav_menu['data']['navigation_menu'][0]['menu_items']
-
-def landing_page(request):
-    """View to return landing Page"""
-    nav_menu = get_menu()
-    params = {
-    'page': '1',
-    'page_size': '10',
-    'levels': '3',
-    }
-    data = Butter.pages.get('*', 'landing-page', params)['data']['fields']
-    return render(request, 'landing_page.html', {
-        'data': data,
-        'nav_menu': nav_menu
-    })
-
-def book_detail_page(request, book_slug=None):
-    """View that grabs detailed book data"""
-    nav_menu = get_menu()
-    params = {
-        'page': '1',
-        'page_size': '20',
-    }
-    if book_slug:
-        book = Butter.pages.get('*', book_slug, params)
-        if 'detail' in book.keys():
-            return HttpResponseNotFound()         
-        else:
-            return render(request, 'book_detail.html', {
-                'book': book,
-                'nav_menu':nav_menu
-            }
-    )
