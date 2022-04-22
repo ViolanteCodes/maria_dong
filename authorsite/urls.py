52a241f8e63383006c092b0e9724b7955f52b266
ViolanteCodes <maria@buttercms.com>
2022-04-21 21:12:24 -0400

    book list view created

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from layout.views import ButterView
+from layout.views import ButterPageView, ButterListView
 
 
 urlpatterns = [
-    path('', ButterView.as_view(template_name="landing_page.html"),
+    path('', ButterPageView.as_view(template_name="landing_page.html"),
         kwargs={'page_slug': 'landing-page'}),
-    path('books/<page_slug>/', ButterView.as_view(template_name='book_detail.html'),
+    path('books/', ButterListView.as_view(template_name="books_list.html"), 
+        kwargs={'page_type': 'book'}, name="books_list"), 
+    path('books/<page_slug>/', ButterPageView.as_view(template_name='book_detail.html'),
         name="book_page"),
     # path('books/<book_slug>/', views.book_detail_page, name="book_detail_page"),
     # path('admin/', admin.site.urls),
