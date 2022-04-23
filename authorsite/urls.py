971eae3e261d7c61695b73f69df3e97e0110a605
ViolanteCodes <maria@buttercms.com>
2022-04-23 13:10:57 -0400

    Add SEO blocks to butter and all completed views

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from layout.views import ButterPageView, ButterListView
+from layout.views import ButterPageView
 
 
 urlpatterns = [
     path('', ButterPageView.as_view(template_name="landing_page.html"),
         kwargs={'page_slug': 'landing-page'}),
-    path('books/', ButterListView.as_view(template_name="books_list.html"), 
-        kwargs={'page_type': 'book'}, name="books_list"), 
+    path('about/', ButterPageView.as_view(template_name="about.html"),
+        kwargs={'page_slug': 'about'}, name="about"),
+    path('books/', ButterPageView.as_view(template_name="books_list.html"), 
+        kwargs={'page_slug': 'books'}, name="books_list"), 
     path('books/<page_slug>/', ButterPageView.as_view(template_name='book_detail.html'),
         name="book_page"),
-    # path('books/<book_slug>/', views.book_detail_page, name="book_detail_page"),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
