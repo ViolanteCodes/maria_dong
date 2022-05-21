63dcb6839cf5952a7328cd8248b170fa7594f7aa
ViolanteCodes <maria@buttercms.com>
2022-05-21 15:06:27 -0400

    update success url

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
         kwargs={'page_slug': 'link-tree'}, name="link_tree"),
     path('about/', ContactFormView.as_view(template_name="about.html"),
         kwargs={'page_slug': 'about'}, name="about"),
-    path('success/', ButterPageView.as_view(template_name="success.html"),
+    path('about/success/', ButterPageView.as_view(template_name="success.html"),
         kwargs={'page_slug': 'success'}, name="sucess"),
     path('books/', ButterPageView.as_view(template_name="books_list.html"), 
         kwargs={'page_slug': 'books'}, name="books_list"), 
