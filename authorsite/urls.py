eecd49bfdde658cd70ccca9fcc53c67e3162fed9
Maria Violante <maria@mviolante.com>
2026-06-25 10:11:30 -0400

    Fix media_root to display images

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
@@ -39,3 +40,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )
