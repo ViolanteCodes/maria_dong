bb993c6ec231e589e4de8bb927a36711de31a39f
Maria Violante <maria@mviolante.com>
2026-06-26 09:03:07 -0400

    cleanup

--- authorsite/urls.py
+++ authorsite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
-from layout.views import ButterPageView, ContactFormView, CampaignFormView
+from layout.views import ButterPageView
 from books.views import BookView
 from books.views import BooksListView
 from shorts.views import ShortsListView
@@ -20,8 +20,8 @@ urlpatterns = [
         {'page_type':'links_page', 'page_slug': 'link-tree'}, name="link_tree"),
     path('about/', AboutView.as_view(), name="about"),
     path('about/success/', SuccessView.as_view(), name="success"),
-    path('campaigns/<page_slug>/', CampaignFormView.as_view(template_name="campaign.html"),
-        {'page_type': 'campaign_page'}, name="campaign"),
+    # path('campaigns/<page_slug>/', CampaignFormView.as_view(template_name="campaign.html"),
+    #     {'page_type': 'campaign_page'}, name="campaign"),
     path('books/', BooksListView.as_view(), name="books_list"), 
     path('books/<slug:slug>/', BookView.as_view(), name="book_page"),
     path('events/', PressView.as_view(), name="events"),
--- layout/views.py
+++ layout/views.py
@@ -83,90 +83,55 @@ class ButterMixin:
         page_type_data = Butter.pages.all(page_type, params)['data']
         return page_type_data
 
-class CampaignViewMixin(ButterMixin):
+# class CampaignViewMixin(ButterMixin):
 
-    def get_page(self, page_type='*', page_slug=None, params={}):
-        """Fetch campaign page from Butter and add tailwind classes"""
-        page_data = Butter.pages.get(page_type, page_slug, params)['data']['fields']
-        print(page_data)
+#     def get_page(self, page_type='*', page_slug=None, params={}):
+#         """Fetch campaign page from Butter and add tailwind classes"""
+#         page_data = Butter.pages.get(page_type, page_slug, params)['data']['fields']
+#         print(page_data)
         
-        if "p" in page_data["campaign_instructions"]:
-            page_data["campaign_instructions"] = page_data["campaign_instructions"].replace("<p>", '<p class="mb-8">')
-        if "ul" in page_data["campaign_instructions"]:
-            page_data["campaign_instructions"] = page_data["campaign_instructions"].replace("<ul>", '<ul class="list-disc">')
-        return page_data
+#         if "p" in page_data["campaign_instructions"]:
+#             page_data["campaign_instructions"] = page_data["campaign_instructions"].replace("<p>", '<p class="mb-8">')
+#         if "ul" in page_data["campaign_instructions"]:
+#             page_data["campaign_instructions"] = page_data["campaign_instructions"].replace("<ul>", '<ul class="list-disc">')
+#         return page_data
     
 class ButterPageView(ButterMixin, TemplateView):
     """Base Butter Page View"""
 
-class ContactFormView(ButterMixin, FormView):
-    template_name = 'about.html'
-    form_class = ContactForm
-    success_url = 'success/'
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        sender_email = form.cleaned_data['sender_email']
-        from_email = settings.CONTACT_EMAIL
-        sender_name = form.cleaned_data['sender_name']
-        sender_subject = form.cleaned_data['sender_subject']
-        message = form.cleaned_data['message']
-        full_message = "You have the following contact form submission from Maria Dong.com:\n\n"""
-        full_message += f"\n\tFrom Sender: {sender_name}."
-        full_message += f"\n\tSender's email: {sender_email}."
-        full_message += f"\n\tSubject: {sender_subject}."
-        full_message += f"\n\tMessage:\n\n\t{message}"
-        message = full_message
-
-        try:
-            # send_mail(subject, message, from_email, [from_email], fail_silently=False)
-            email = EmailMessage(
-                subject=sender_subject,
-                body=message,
-                from_email=from_email,
-                to=[from_email],
-                reply_to=[sender_email]
-            )
-            email.send()
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return super().form_valid(form)
-        
-def successView(request):
-    return render(request, 'contact/success.html')
-
-class CampaignFormView(CampaignViewMixin, FormView):
-    template_name = 'campaign.html'
-    form_class = CampaignForm
-    success_url = reverse_lazy('success')
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        sender_email = form.cleaned_data['sender_email']
-        from_email = settings.CONTACT_EMAIL
-        sender_name = form.cleaned_data['sender_name']
-        campaign_name = form.cleaned_data['campaign_name']
-        image_field = form.cleaned_data['image_field']
-        sender_subject = f"New campaign entry: {campaign_name}"
-        full_message = f"You have the following entry for the campaign: {campaign_name}:\n\n"""
-        full_message += f"\n\tFrom Sender: {sender_name}."
-        full_message += f"\n\tSender's email: {sender_email}."
-        message = full_message
 
 
-        try:
-            # send_mail(subject, message, from_email, [from_email], fail_silently=False)
-            email = EmailMessage(
-                subject=sender_subject,
-                body=message,
-                from_email=from_email,
-                to=[from_email],
-                reply_to=[sender_email]
-            )
-            email.attach(image_field.name, image_field.read(), image_field.content_type)
-            email.send()
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return super().form_valid(form)
+# class CampaignFormView(CampaignViewMixin, FormView):
+#     template_name = 'campaign.html'
+#     form_class = CampaignForm
+#     success_url = reverse_lazy('success')
+
+#     def form_valid(self, form):
+#         # This method is called when valid form data has been POSTed.
+#         # It should return an HttpResponse.
+#         sender_email = form.cleaned_data['sender_email']
+#         from_email = settings.CONTACT_EMAIL
+#         sender_name = form.cleaned_data['sender_name']
+#         campaign_name = form.cleaned_data['campaign_name']
+#         image_field = form.cleaned_data['image_field']
+#         sender_subject = f"New campaign entry: {campaign_name}"
+#         full_message = f"You have the following entry for the campaign: {campaign_name}:\n\n"""
+#         full_message += f"\n\tFrom Sender: {sender_name}."
+#         full_message += f"\n\tSender's email: {sender_email}."
+#         message = full_message
+
+
+#         try:
+#             # send_mail(subject, message, from_email, [from_email], fail_silently=False)
+#             email = EmailMessage(
+#                 subject=sender_subject,
+#                 body=message,
+#                 from_email=from_email,
+#                 to=[from_email],
+#                 reply_to=[sender_email]
+#             )
+#             email.attach(image_field.name, image_field.read(), image_field.content_type)
+#             email.send()
+#         except BadHeaderError:
+#             return HttpResponse('Invalid header found.')
+#         return super().form_valid(form)
